68dfcf4e78d300b505487309a4c7c1502fc8850b
Gowri Nambiar <gowri.nambi@gmail.com>
2013-11-26 16:54:36 -0500

    fixed last and uncommented

--- jquery_exercises.js
+++ jquery_exercises.js
@@ -1,140 +1,138 @@
-// SOLUTIONS GO BELOW EACH EXERCISE
+SOLUTIONS GO BELOW EACH EXERCISE
 
-// // EXERCISE: Change Background
+// EXERCISE: Change Background
 
-// // Change the background color of '#target' by script. 
-// // Hint: you can use .css
-// $(document).ready(function() {
-// 	$('div').css('background-color', 'blue')
-// });
+// Change the background color of '#target' by script. 
+// Hint: you can use .css
+$(document).ready(function() {
+	$('div').css('background-color', 'blue')
+});
 
-// // EXERCISE: Change Parent
+// EXERCISE: Change Parent
 
-// // Change the text in the span, a child of "#target"
-// // Hint: you can use .text
-// $(document).ready(function() {
-// 	$('span').text('lovely')
-// });
+// Change the text in the span, a child of "#target"
+// Hint: you can use .text
+$(document).ready(function() {
+	$('span').text('lovely')
+});
 
-// // EXERCISE: Create Clone
+// EXERCISE: Create Clone
 
-// // Create a clone of the span in "#target" and position it under the origin.
-// // Hint: you can use .clone and .insertAfter
+// Create a clone of the span in "#target" and position it under the origin.
+// Hint: you can use .clone and .insertAfter
 
-// $(document).ready(function() {
-// 	$('span').clone().insertAfter('span')
-// })
-
-
-// // EXERCISE: Use Filter
-
-// // Change background color of the second ".target"
-// // Hint: you can use .eq and .css
-// $(document).ready(function(){
-// 	$('span').eq(1).css('background-color', 'green')
-// })
+$(document).ready(function() {
+	$('span').clone().insertAfter('span')
+})
 
 
-// // EXERCISE: Disable Buttons
+// EXERCISE: Use Filter
 
-// // Disable the button
-// // Hint: you will have to select the button and can use .attr
-
-// $(document).ready(function() {
-// 	$('button').attr("disabled", "disabled")
-// });
-
-
-// // EXERCISE: Uncheck CheckBoxes
-
-// // Uncheck all checkboxes using jQuery
-// // Hint: you will have to select the input and can use .removeAttr
-//  $(document).ready(function() {
-//  		$('input').removeAttr('checked')
-//  });
-
-// // EXERCISE: Change Parent
-
-// // Move "#child" from "#parent1" to "#parent2"
-// // Hint: you can use .appendTo
-// $(document).ready(function(){
-// 	$("#child").appendTo("#parent2")
-// });
+// Change background color of the second ".target"
+// Hint: you can use .eq and .css
+$(document).ready(function(){
+	$('span').eq(1).css('background-color', 'green')
+})
 
 
-// // EXERCISE: Select A Option in A Select Box
+// EXERCISE: Disable Buttons
 
-// // Select the second option in the selectbox by script
-// // Hint: you will have to select the option and can use .eq and .attr
+// Disable the button
+// Hint: you will have to select the button and can use .attr
 
-// $(document).ready(function(){
-// 	$('option').eq(1).attr("selected","selected")
-// });
-
-// // EXERCISE: Change Size
-
-// // Make "#target" double size
-// // Hint: you can use .css, .width and .height
-// $(document).ready(function(){
-// 	$('#target').width(300).height(200)
-// });
-
-// EXERCISE: Empty Elements
-// Hint: you can use .empty
-// Remove all children and text of "#target"
-
-// $(document).ready(function(){
-// 	$('#target').empty()
-// });
-
-// EXERCISE: Delay
-
-// Show Alert with 1 second delay (Use "setTimeout")
-// Hint: you can use .setTimeout and alert
-//$(document).ready(function(){
-	//alert('alert').setTimeout(1000)
-//})
-
-// EXERCISE: Count
-
-// Show the number of children in an alert
-// Hint: you can use .children, .length and alert or you could use .find, .size and alert. the alert should show 5
-
-//$(document).ready(function(){
-	//alert('5').children().length()
-//});
+$(document).ready(function() {
+	$('button').attr("disabled", "disabled")
+});
 
 
+// EXERCISE: Uncheck CheckBoxes
 
-// EXERCISE: Animate
+// Uncheck all checkboxes using jQuery
+// Hint: you will have to select the input and can use .removeAttr
+ $(document).ready(function() {
+ 		$('input').removeAttr('checked')
+ });
 
-// Make "#target" double size with animation
-// Hint: you can use .animate, .width, and .height
+// EXERCISE: Change Parent
 
-// $(document).ready(function(){
-// 	$('#target').animate('#target').width(300).height(200)
-// });
-
-// EXERCISE: Alternate Color
-
-// Make the list-box alternating. (Make odd options a different color)
-// Hint: you can select the option and use .filter and .css
-
-// $(document).ready(function(){
-// 	$('option').filter(':odd').css('background-color', 'green')
-// });
+// Move "#child" from "#parent1" to "#parent2"
+// Hint: you can use .appendTo
+$(document).ready(function(){
+	$("#child").appendTo("#parent2")
+});
 
 
-// EXERCISE: All But One
+// EXERCISE: Select A Option in A Select Box
 
-// Remove all children of "#target" but h2
-// Hint: you can use .children, .not and .remove
+// Select the second option in the selectbox by script
+// Hint: you will have to select the option and can use .eq and .attr
 
-// $(document).ready(function(){
-// 	$('#target').children().not('h2').remove()
-// });
+$(document).ready(function(){
+	$('option').eq(1).attr("selected","selected")
+});
+
+// EXERCISE: Change Size
+
+// Make "#target" double size
+// Hint: you can use .css, .width and .height
+$(document).ready(function(){
+	$('#target').width(300).height(200)
+});
+
+EXERCISE: Empty Elements
+Hint: you can use .empty
+Remove all children and text of "#target"
+
+$(document).ready(function(){
+	$('#target').empty()
+});
+
+EXERCISE: Delay
+
+Show Alert with 1 second delay (Use "setTimeout")
+Hint: you can use .setTimeout and alert
+$(document).ready(function(){
+	alert('alert').setTimeout(1000)
+})
+
+EXERCISE: Count
+
+Show the number of children in an alert
+Hint: you can use .children, .length and alert or you could use .find, .size and alert. the alert should show 5
+
+$(document).ready(function(){
+	alert('5').children().length()
+});
 
 
+
+EXERCISE: Animate
+
+Make "#target" double size with animation
+Hint: you can use .animate, .width, and .height
+
+$(document).ready(function(){
+	$('#target').animate('#target').width(300).height(200)
+});
+
+EXERCISE: Alternate Color
+
+Make the list-box alternating. (Make odd options a different color)
+Hint: you can select the option and use .filter and .css
+
+$(document).ready(function(){
+	$('option').filter(':odd').css('background-color', 'green')
+});
+
+
+EXERCISE: All But One
+
+Remove all children of "#target" but h2
+Hint: you can use .children, .not and .remove
+
+$(document).ready(function(){
+	$('#target').children().not('h2').remove()
+});
 
 
 // EXERCISE: Without Children
@@ -143,6 +141,6 @@
 // Hint: you can use .not and .remove. Look closely at what options you can pass .not
 
 $(document).ready(function(){
-	$('div').not('span').remove
+	$('div').not('div:contains("span")').remove()
 });
-/////////////////////////////
+
